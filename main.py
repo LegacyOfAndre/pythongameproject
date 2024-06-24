d2dbb54c57dac55c67c9e82645a816cc27fd3ce9
legacyofandre <andreluispcamilles1999@hotmail.com>
2024-06-24 15:18:11 -0300

    Signed-off-by: legacyofandre <andreluispcamilles1999@hotmail.com>

--- main.py
+++ main.py
@@ -8,20 +8,19 @@ WINDOW_HEIGHT = 324
 pygame.init()
 print("starting setup")
 # Criando janela no PyGame
-window :Surface = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
+window: Surface = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
 
 # Carregando imagem e gerando uma superfície
-bg_surf :Surface = pygame.image.load("asset/background/orig.png").convert_alpha()
-player_surf :Surface = pygame.image.load("./asset/ship/Ship1.png").convert_alpha()
+bg_surf: Surface = pygame.image.load("asset/background/orig.png").convert_alpha()
+player_surf: Surface = pygame.image.load("./asset/ship/Ship1.png").convert_alpha()
 
 # Obtendo retangulo da superficie
 bg_rect: Rect = bg_surf.get_rect(left=0, top=0)
 player_rect: Rect = player_surf.get_rect(left=100, top=100)
 
-
 # Desenhando a imagem na janela (Window)
-window.blit(source= bg_surf, dest=bg_rect)
-window.blit(source= player_surf, dest=player_rect)
+window.blit(source=bg_surf, dest=bg_rect)
+window.blit(source=player_surf, dest=player_rect)
 
 # Atualizando a janela
 pygame.display.flip()
@@ -34,12 +33,11 @@ pygame.mixer_music.load("./asset/soundsambience/475620__tyops__game-intro-space-
 pygame.mixer_music.play(-1)
 pygame.mixer_music.set_volume(0.3)
 
-
 print("ending setup")
 print("Loop starting")
 while True:
-    clock.tick(140) # Este loop acontece 60 vezes por segundo ou 60 FPS
-    print(f"{clock.get_fps() :.0f}") # executando e printando o valor do FPS em tempo real no console
+    clock.tick(140)  # Este loop acontece 60 vezes por segundo ou 60 FPS
+    print(f"{clock.get_fps() :.0f}")  # executando e printando o valor do FPS em tempo real no console
 
     window.blit(source=bg_surf, dest=bg_rect)
     window.blit(source=player_surf, dest=player_rect)
